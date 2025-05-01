45e3916346706bddb8625d1951b24d5fbf707f7a
ilodev <ilodev@users.noreply.github.com>
2025-05-01 09:56:27 +0200

    Added small grid cells visualizer on endpoints

---
+++ Editor/Visualizers/SmallGridConnectionCellsVisualizer.cs
@@ -0,0 +1,47 @@
+using Assets.Scripts.Objects;
+using UnityEditor;
+using UnityEngine;
+
+namespace ilodev.stationeersmods.tools.visualizers
+{
+
+    public class SmallGridConnectionsVisualizer : IThingVisualizer
+    {
+        public void OnSceneGUI(SceneView sceneView, Object target)
+        {
+            if (!EditorPrefs.GetBool("Visualizer.Endpoints", true))
+                return;
+
+            SmallGrid smallGrid = target as SmallGrid;
+            if (smallGrid == null)
+                return;
+
+            foreach (var openEnd in smallGrid.OpenEnds)
+            {
+                if (openEnd == null || openEnd.Transform == null)
+                    continue;
+
+                Handles.color = new Color(1.0f, 0.5f, 0.0f, 1.0f); // Orange
+                Vector3 snappedPos = SnapToGrid(openEnd.Transform.position, 0.5f, 0.25f);
+                Handles.DrawWireCube(snappedPos, Vector3.one * 0.5f);
+            }
+        }
+
+        Vector3 SnapToGrid(Vector3 position, float gridSize, float offset)
+        {
+            /*
+            return new Vector3(
+                Mathf.Floor((position.x - offset) / gridSize) * gridSize + offset,
+                Mathf.Floor((position.y - offset) / gridSize) * gridSize + offset,
+                Mathf.Floor((position.z - offset) / gridSize) * gridSize + offset
+            );
+            */
+            return new Vector3(
+                Mathf.Round(position.x / gridSize) * gridSize,
+                Mathf.Round(position.y / gridSize) * gridSize,
+                Mathf.Round(position.z / gridSize) * gridSize
+            );
+
+        }
+    }
+}
