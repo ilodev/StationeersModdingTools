e6fe020fa20b8e2deee776a731af3401e1e9fc6a
ilodev <ilodev@users.noreply.github.com>
2025-05-06 14:50:17 +0200

    Added wireframe inspector to add edges to the WireFrame script

---
+++ Editor/UIHelpers/WireFrameInspector.cs
@@ -0,0 +1,28 @@
+using Assets.Scripts.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace ilodev.stationeersmods.tools.uihelpers
+{
+
+    [CustomEditor(typeof(Wireframe), true)]
+    public class WireFrameInspector : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            Wireframe wf = (Wireframe)target;
+
+            if (GUILayout.Button("Generate Edges"))
+            {
+                WireframeGenerator gen = new WireframeGenerator(wf.transform);
+                wf.WireframeEdges = gen.Edges;
+            }
+
+        }
+    }
+}
