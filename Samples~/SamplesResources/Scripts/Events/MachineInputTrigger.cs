6976664f51929ddc853358d40a9c2a3110d0c551
ilodev <ilodev@users.noreply.github.com>
2025-05-15 20:31:01 +0200

    Added MachineInputTrigger to add the chute triggers to the prefab

---
+++ Samples~/SamplesResources/Scripts/Events/MachineInputTrigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Stationeers.ModdingHelpers
+{
+#if UNITY_EDITOR
+    [AddComponentMenu("Stationeers/Events/Machine Input Trigger")]
+#endif
+    public class MachineInputTrigger : Assets.Scripts.Events.MachineInputTrigger
+    {
+
+    }
+}
