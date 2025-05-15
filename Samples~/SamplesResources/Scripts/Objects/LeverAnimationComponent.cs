b01ac46ec8ba0945bea333afd4731981d739d5e0
ilodev <ilodev@users.noreply.github.com>
2025-05-15 13:16:06 +0200

    Added lever prefab

---
+++ Samples~/SamplesResources/Scripts/Objects/LeverAnimationComponent.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Assets.Scripts.Generated
+{
+#if UNITY_EDITOR
+    [AddComponentMenu("Stationeers/Animation Components/Lever Animation Component")]
+#endif
+    public class LeverAnimationComponent : global::LeverAnimationComponent
+    {
+    }
+}
