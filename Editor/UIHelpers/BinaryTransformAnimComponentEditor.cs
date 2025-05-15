1f478d7563e8655bd2c5e1de96898292533a335f
ilodev <ilodev@users.noreply.github.com>
2025-05-15 19:04:09 +0200

    Added first pass for assigning items to Thing, needs refactoring

--- Editor/UIHelpers/BinaryTransformAnimComponentEditor.cs
+++ Editor/UIHelpers/BinaryTransformAnimComponentEditor.cs
@@ -1,4 +1,8 @@
+using Assets.Scripts.Objects;
+using System;
+using System.Linq;
 using System.Reflection;
+using Trading;
 using UnityEditor;
 using UnityEngine;
 
@@ -26,6 +30,77 @@ namespace ilodev.stationeersmods.tools.uihelpers
             timeProp = serializedObject.FindProperty("time");
  
             EditorApplication.update += UpdateAnimation;
+
+            EditorApplication.contextualPropertyMenu += OnPropertyContextMenu;
+        }
+
+        private void OnPropertyContextMenu(GenericMenu menu, SerializedProperty property)
+        {
+            if (target == null)
+                return;
+
+            if (property.name != "parentThing")
+                return;
+
+            // Parent already setup
+            GameObject parentThing = property.objectReferenceValue as GameObject;
+            if (parentThing != null)
+                return;
+
+            Thing[] things = ((BinaryTransformAnimComponent)target).GetComponentsInParent<Thing>();
+            if (things.Length == 0) 
+                return;
+
+            Thing parent = things[0];
+            if (parent == null) 
+                return;
+
+            menu.AddItem(new GUIContent("Assign parent Thing"), false, () =>
+            {
+                Debug.Log("Testing");
+                AssignParentThing(parent, target);
+            });
+
+        }
+
+        private void AssignParentThing(Thing parent, UnityEngine.Object target)
+        {
+            BinaryTransformAnimComponent BTAC = (BinaryTransformAnimComponent)target;
+            foreach (Transform child in BTAC.transform)
+            {
+                string name = child.gameObject.name;
+
+                #region Assign SmallGrid Connections
+                SmallGrid smallGrid = parent.GetComponent<SmallGrid>();
+                if (smallGrid != null)
+                {
+                    if (name.Contains("Connection")) {
+                        Connection connection = new Connection(smallGrid);
+                        if (name.Contains("Chute"))
+                            connection.ConnectionType = NetworkType.Chute;
+
+                        if (name.Contains("Input"))
+                            connection.ConnectionRole = ConnectionRole.Input;
+
+                        if (name.Contains("Output"))
+                            connection.ConnectionRole = ConnectionRole.Output;
+
+                        connection.Transform = child.transform;
+
+                        Collider collider = child.GetComponent<Collider>();
+                        if (collider != null)
+                            connection.Collider = collider;
+
+                        smallGrid.OpenEnds.Add(connection);
+                    }
+                }
+                #endregion
+
+                #region Assing Lever 
+                #endregion
+            }
+
+            BTAC.SetParentThing(parent);
         }
 
         private void OnDisable()
@@ -34,6 +109,7 @@ namespace ilodev.stationeersmods.tools.uihelpers
             // Reset position to state0
             ApplyState(state0);
             EditorApplication.update -= UpdateAnimation;
+            EditorApplication.contextualPropertyMenu -= OnPropertyContextMenu;
         }
 
         private void ApplyState(FieldInfo state)
